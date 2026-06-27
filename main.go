5e3f2ee8440217a9c3166e9e8db3c77e00cd2d79
Nikita Rod() <bira10a@yandex.ru>
2026-06-27 15:30:17 +0300

    второй коммит

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import "fmt"
 
+const USD = 1
+
 func main() {
-	const USD = 1
 	const EUR = USD * 1.16
 	const RUB = USD * 72.14
 
 	EURfromRUM := RUB / EUR
 
 	fmt.Println(EURfromRUM)
+
+	value := userValue()
+	fmt.Println(value)
+
+}
+
+func userValue() float64 {
+	var value float64
+	fmt.Scan(&value)
+	return value
+}
+
+func converter(value, evro, rub float64) float64 {
+	var USD = value
+	var EUR = USD * evro
+	var RUB = USD * rub
+
+	var converterEURtoRub = RUB / EUR
+	return converterEURtoRub
 }

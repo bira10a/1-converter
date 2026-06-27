bc21dd03085bb478df3cc108d97ca8c798452953
Nikita Rod() <bira10a@yandex.ru>
2026-06-27 16:43:55 +0300

    третий коммит

--- main.go
+++ main.go
@@ -3,31 +3,101 @@ package main
 import "fmt"
 
 const USD = 1
+const EUR = USD * 1.16
+const RUB = USD * 72.14
 
 func main() {
-	const EUR = USD * 1.16
-	const RUB = USD * 72.14
 
 	EURfromRUM := RUB / EUR
 
 	fmt.Println(EURfromRUM)
 
-	value := userValue()
-	fmt.Println(value)
+	sum := converter()
+	fmt.Println(sum)
 
 }
 
-func userValue() float64 {
+func converter() (sum float64) {
+
+	var firstVal string
+	var lastVal string
 	var value float64
-	fmt.Scan(&value)
-	return value
-}
 
-func converter(value, evro, rub float64) float64 {
-	var USD = value
-	var EUR = USD * evro
-	var RUB = USD * rub
+	for {
+		fmt.Print("Выберите исходную валюту:")
+		fmt.Scan(&firstVal)
 
-	var converterEURtoRub = RUB / EUR
-	return converterEURtoRub
+		switch firstVal {
+		case "USD":
+			fmt.Println("Вы выбрали доллары")
+		case "EUR":
+			fmt.Println("Вы выбрали евро")
+		case "RUB":
+			fmt.Println("Вы выбрали рубли")
+		default:
+			fmt.Println("Выберите исходную валюту:")
+			fmt.Scan(&firstVal)
+		}
+
+		break
+	}
+
+	for {
+		fmt.Print("Выберите число:")
+		fmt.Scan(&value)
+
+		if value == 0 || value < 0 {
+			fmt.Print("Выберите число pliz:")
+			fmt.Scan(&value)
+		}
+		fmt.Println("Вы выбрали", value)
+		break
+	}
+
+	for {
+		fmt.Print("Выберите вторую валюту:")
+		fmt.Scan(&lastVal)
+
+		if firstVal == lastVal {
+			fmt.Println("Выберите вторую валюту, отличную от первой:")
+			fmt.Scan(&lastVal)
+		}
+
+		switch lastVal {
+		case "USD":
+			fmt.Println("Вы выбрали второй валютой доллары")
+		case "EUR":
+			fmt.Println("Вы выбрали второй валютой евро")
+		case "RUB":
+			fmt.Println("Вы выбрали второй валютой рубли")
+		default:
+			fmt.Println("Выберите вторую валюту:")
+			fmt.Scan(&firstVal)
+		}
+
+		break
+	}
+
+	if firstVal == "USD" && lastVal == "EUR" {
+		sum = (USD / 1.16) * value
+		return
+	} else if firstVal == "USD" && lastVal == "RUB" {
+		sum = (USD * 72.14) * value
+		return
+	} else if firstVal == "EUR" && lastVal == "USD" {
+		sum = (USD * 1.16) * value
+		return
+	} else if firstVal == "EUR" && lastVal == "RUB" {
+		sum = (USD * 72.14 * 1.16) * value
+		return
+	} else if firstVal == "RUB" && lastVal == "USD" {
+		sum = (USD / 72.14) * value
+		return
+	} else if firstVal == "RUB" && lastVal == "EUR" {
+		sum = (USD / (72.14 * 1.16)) * value
+		return
+	}
+
+	return
+
 }
